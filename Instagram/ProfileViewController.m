c65b00d39342df752491946ea87e3e2a9c8ee3d9
Alex <alexsantorineos@gmail.com>
2015-06-08 16:52:39 -0500

    view controller sign up

---
+++ Instagram/ProfileViewController.m
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.m
+//  Instagram
+//
+//  Created by Alex Santorineos on 6/8/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
