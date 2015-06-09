b865dc2743b90a146c0ba4fbe113a6870584020f
Alex <alexsantorineos@gmail.com>
2015-06-08 22:55:09 -0500

    implemented photos

--- Instagram/CaptureViewController.m
+++ Instagram/CaptureViewController.m
@@ -8,10 +8,12 @@
 
 #import "CaptureViewController.h"
 #import <MobileCoreServices/UTCoreTypes.h>
+#import <Parse/Parse.h>
 
 @interface CaptureViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property UIImage *image;
 @property NSString *videoFilePath;
+@property NSArray *images;
 
 @end
 
@@ -24,6 +26,8 @@
     self.imagePicker = [[UIImagePickerController alloc]init];
     self.imagePicker.delegate  = self;
     self.imagePicker.allowsEditing =YES;
+    self.imagePicker.videoMaximumDuration = 16;
+
 
     //checks if camera is accesible
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
@@ -44,10 +48,12 @@
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:true];
 
+    if (self.image == nil && [self.videoFilePath length] == 0) {
 
     self.imagePicker = [[UIImagePickerController alloc]init];
     self.imagePicker.delegate  = self;
     self.imagePicker.allowsEditing =YES;
+    self.imagePicker.videoMaximumDuration = 16;
 
     //checks if camera is accesible
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
@@ -63,7 +69,7 @@
 
     [self presentViewController:self.imagePicker animated:true completion:nil];
 
-
+    }
 }
 
 
@@ -78,7 +84,7 @@
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
-
+    //if media is equeal to image
     if ([mediaType isEqual:(NSString *)kUTTypeImage]) {
         //a photo was taken
         self.image = [info objectForKey:UIImagePickerControllerEditedImage];
@@ -96,18 +102,96 @@
 
                 //check if you can put a video in camera roll
                 if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(self.videoFilePath)) {
+                    //save the video
                     UISaveVideoAtPathToSavedPhotosAlbum(self.videoFilePath, nil, nil, nil);
-
                 }
-                //save the video
 
             }
+
+        [self uploadPic];
         [self dismissViewControllerAnimated:NO completion:nil];
         [self.tabBarController setSelectedIndex:0];
     }
 
-//    [self dismissViewControllerAnimated:NO completion:nil];
-//    [self.tabBarController setSelectedIndex:0];
-
 }
+
+
+
+-(void)uploadPic{
+    //check if its an image or video
+    NSData *fileData;
+    NSString *fileName;
+    NSString *fileType;
+
+
+
+    if (self.image !=nil) {
+        UIImage *newImage = self.image;
+
+        //data is png
+//        fileData = UIImagePNGRepresentation(newImage);
+        fileData = UIImageJPEGRepresentation(newImage, 0.5);
+        fileName = @"image";
+        fileType = @"image";
+    }
+
+    else{
+        //data type is video
+        fileData=[NSData dataWithContentsOfFile:self.videoFilePath];
+        fileName = @"video.mov";
+        fileType = @"video";
+
+    }
+
+    PFFile *file = [PFFile fileWithName:fileName data:fileData];
+
+    [file saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+
+
+
+        if (error) {
+            UIAlertView *alertview = [[UIAlertView alloc]initWithTitle:@"error occured" message:@"please attempt to send ur message again, 😜" delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+
+            [alertview show];
+        }
+
+        else{
+
+            //file is on parse.com!!!
+
+            PFObject *picObject = [PFObject objectWithClassName:@"PictureUpload"];
+            [picObject setObject:file forKey:@"file"];
+            [picObject setObject:fileType forKey:@"fileType"];
+            [picObject setObject:[[PFUser currentUser]objectId] forKey:@"UserId"];
+            [picObject setObject:[[PFUser currentUser]username] forKey:@"userName"];
+
+             [picObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (error) {
+                    UIAlertView *alertview = [[UIAlertView alloc]initWithTitle:@"error occured" message:@"please attempt  upload again" delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+                    [alertview show];
+                    }else{
+                    //everything was succesful
+
+//                        for (PFObject *object in self.images) {
+//                        self.images = [NSArray arrayWithObject:object];
+//                            NSLog(@"😜😜😜😜%@😜😜😜😜",object);
+//                        }
+//                        self.images = [NSArray arrayWithObjects:picObject, nil];
+//
+//                        NSLog(@"%@😜😜😜😜",self.images);
+                    [self reset];
+                        }
+                            }];
+
+                        }
+        
+                    }];
+                }
+
+- (void)reset {
+    self.image=nil;
+    self.videoFilePath=nil;
+}
+
+
 @end
