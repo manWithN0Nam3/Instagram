889d50b5b61b7669c13e1c9194ef365e01c1603f
Tom Carmona <thomascarmona@gmail.com>
2015-06-10 15:32:07 -0500

    storyboard:)

---
+++ Instagram/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  Instagram
+//
+//  Created by Tom Carmona on 6/10/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewController : UIViewController
+
+@end
