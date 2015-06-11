c840dd373c739c7f57945adf393b0d5703bccca2
Tom Carmona <thomascarmona@gmail.com>
2015-06-11 15:27:37 -0500

    Updated home feed w/ feedcell class and populated pictures array properly

---
+++ Instagram/FeedTableViewCell.h
@@ -0,0 +1,29 @@
+//
+//  FeedTableViewCell.h
+//  Instagram
+//
+//  Created by Tom Carmona on 6/11/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *feedImageView;
+//
+//@property (weak, nonatomic) IBOutlet UIButton *commentsButton;
+//
+//@property (weak, nonatomic) IBOutlet UIImageView *likeImageView;
+//
+//@property (weak, nonatomic) IBOutlet UIImageView *commentImageView;
+//
+//@property (weak, nonatomic) IBOutlet UIImageView *shareImageView;
+//
+//@property (weak, nonatomic) IBOutlet UIImageView *thumbnailImageView;
+//
+//@property (weak, nonatomic) IBOutlet UIButton *usernameLabel;
+//
+//@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
+
+
+@end
